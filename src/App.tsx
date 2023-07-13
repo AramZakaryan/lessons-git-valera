f4b39a63cdf5d3a16a7a0bff41646cee4a2c0ece
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-07-13 15:30:34 +0400

    add test, user and post components

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo"/>
                 <title>AddedTitle</title>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
