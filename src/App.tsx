e7f87aabb3ee31f6653fa33d7d16448cd2a0a816
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-07-13 17:38:21 +0400

    situation new new

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <h2>AddedTitle nez</h2>
+                <h2>AddedTitle nezzzzzz</h2>
                 <p>
                      Edit <code>src/App.tsx</code> and save to reload.
                 </p>
